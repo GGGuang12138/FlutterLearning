2fb538a19275183d574b5cf33cd475d98d4a606e
Gg <ggguang@GgdeMacBook-Air.local>
2020-03-18 14:31:01 +0800

    第一讲，理解Widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First Flutter App'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('hello world'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text('Button'),
+          onPressed: (){
+            print('test');
+          },
+        ),
+      ),
     );
 
 
