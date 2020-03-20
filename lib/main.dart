bc5a37f50f141f7218b118f36cd65661f95d156c
Gg <ggguang@GgdeMacBook-Air.local>
2020-03-20 11:13:31 +0800

    绘制卡片

--- lib/main.dart
+++ lib/main.dart
@@ -20,38 +20,79 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My First Flutter App'),
-        centerTitle: true,
-        backgroundColor: Colors.red[300],
-      ),
-      body: Row(children: <Widget>[
-        Expanded(
-          flex: 1,
-          child: Container(
-            child: Image.asset('assets/anzhuo.png'),
-            color: Colors.red[50],
-          ),
+      backgroundColor: Colors.grey[200],
+      // appBar: AppBar(
+      //   title: Text(
+      //     'My First Flutter App',
+      //     style: TextStyle(color: Colors.grey[900]),
+      //   ),
+      //   centerTitle: true,
+      //   backgroundColor: Colors.grey[200],
+      // ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(30, 80, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/anzhuo.png'),
+                radius: 70,
+                backgroundColor: Colors.grey[300],
+              ),
+            ),
+            Divider(
+              height: 60,
+              color: Colors.grey[600],
+              thickness: 2,
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'GG Chen',
+              style: TextStyle(
+                color: Colors.black,
+                letterSpacing: 2,
+                fontSize: 28,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'favorite',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Coding',
+              style: TextStyle(
+                  color: Colors.black, letterSpacing: 2, fontSize: 28),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(Icons.email, color: Colors.green[600]),
+                SizedBox(width: 5),
+                Text(
+                  'ggguang12138@icloud.com',
+                  style: TextStyle(
+                      color: Colors.grey[700], fontSize: 18, letterSpacing: 0.5),
+                )
+              ],
+            ),
+          ],
         ),
-        Expanded(
-          flex: 2,
-          child: Container(
-            child: Text("one"),
-            padding: EdgeInsets.all(30),
-            color: Colors.red[100],
-          ),
-        ),
-        Container(
-          child: Text("two"),
-          padding: EdgeInsets.all(40),
-          color: Colors.red[200],
-        )
-      ]),
-      floatingActionButton: FloatingActionButton(
-        child: Text('Button'),
-        onPressed: () {
-          print('test');
-        },
       ),
     );
   }
