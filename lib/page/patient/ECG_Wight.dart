c28be4c7a50f18a857f61e21057f3e140a0e271c
Gg <ggguang@GgdeMacBook-Air.local>
2020-04-18 10:59:30 +0800

    path画直线图加动画

--- lib/page/patient/ECG_Wight.dart
+++ lib/page/patient/ECG_Wight.dart
@@ -1,24 +1,158 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 //import 'dart:math' as math;
 
 //存点的列表
-class DrawingPoints {
-  Paint paint;
-  Offset points;
-  DrawingPoints({this.points, this.paint});
+// class DrawingPoints {
+//   Paint paint;
+//   Offset points;
+//   DrawingPoints({this.points, this.paint});
+// }
+
+//数据
+class Point {
+  double y;
+
+  Point({@required this.y});
 }
 
 class DemoPainter extends CustomPainter {
-  List<DrawingPoints> pointsList;
   List<Offset> offsetPoints = List();
   List<Offset> points = List();
 
+  double startX, endX, startY, endY; //定义绘制区域的边界
+  static const double basePadding = 16; //默认的边距
+  double fixedHeight, fixedWidth; //去除padding后曲线的真实宽高
+  bool isShowXyRuler; //是否显示xy刻度
+  List<Point> chatBeans = []; //数据源
 
+  Size size;
+  int yNum, length;
+  List<double> maxMin;
+  Path path;
+  double value;
+
+  DemoPainter({@required this.chatBeans,@required this.value});
+
+  void initBorder(Size size) {
+    print('size - - > $size');
+    yNum = chatBeans.length;
+    startX = yNum > 0 ? basePadding * 2.5 : basePadding * 2; //预留出y轴刻度值所占的空间
+    endX = size.width - basePadding * 2;
+    startY = size.height - (basePadding);
+    endY = basePadding * 2;
+    fixedHeight = startY - endY;
+    fixedWidth = endX - startX;
+    maxMin = calculateMaxMin(chatBeans);
+  }
+
+  //  求极值
+  List<double> calculateMaxMin(List<Point> chatBeans) {
+    if (chatBeans == null || chatBeans.length == 0) return [0, 0];
+    double max = 0.0, min = 0.0;
+    for (Point bean in chatBeans) {
+      if (max < bean.y) {
+        max = bean.y;
+      }
+      if (min > bean.y) {
+        min = bean.y;
+      }
+    }
+    return [max, min];
+  }
 
   @override
   void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..color = Colors.green
+      ..strokeWidth = (1.0)
+      ..isAntiAlias = true;
+
+    //画坐标
+    initBorder(size);
+
+    //画x轴
+    length = chatBeans.length; //最多绘制7个
+    print(length.toString());
+    double DW = fixedWidth / (length - 1); //两个点之间的x方向距离
+    double DH = fixedHeight / (length - 1); //两个点之间的y方向距离
+    // for (int i = 0; i < length; i++) {
+    //   ///绘制x轴文本
+    //   TextPainter(
+    //         textAlign: TextAlign.center,
+    //         ellipsis: '.',
+    //         text: TextSpan(
+    //             text: chatBeans[i].x,
+    //             style: TextStyle(color: Colors.black, fontSize: 12)),
+    //         textDirection: TextDirection.ltr)
+    //       ..layout(minWidth: 40, maxWidth: 40)
+    //       ..paint(canvas, Offset(startX + DW * i - 20, startY + basePadding));
+
+    //   ///x轴刻度
+    //   //canvas.drawLine(Offset(startX + DW * i, startY),Offset(startX + DW * i, startY - rulerWidth), paint);
+    // }
+
+    //画y轴
+    // int yLength = yNum + 1; //包含原点,所以 +1
+    // double dValue = maxMin[0] / yNum; //一段对应的值
+    // double dV = fixedHeight / yNum; //一段对应的高度
+    // for (int i = 0; i < yLength; i++) {
+    //   ///绘制y轴文本，保留1位小数
+    //   var yValue = (dValue * i).toStringAsFixed(0);
+    //   TextPainter(
+    //       textAlign: TextAlign.center,
+    //       ellipsis: '.',
+    //       maxLines: 1,
+    //       text: TextSpan(
+    //           text: '$yValue',
+    //           style: TextStyle(color: Colors.black, fontSize: 12)),
+    //       textDirection: TextDirection.rtl)
+    //     ..layout(minWidth: 40, maxWidth: 40)
+    //     ..paint(canvas, Offset(startX - 40, startY - dV * i - 12 / 2));
+
+    //   ///y轴刻度
+    //   //canvas.drawLine(Offset(startX, startY - dV * (i)),Offset(startX + rulerWidth, startY - dV * (i)), paint);
+    // }
+
+    //遍历数组，讲数据填入path
+    path = Path();
+    double preX, preY, currentX, currentY;
+    double W = fixedWidth / (length - 1);
+    for (int i = 0; i < chatBeans.length; i++) {
+      if (i == 0) {
+        path.moveTo(
+            startX, (startY - chatBeans[i].y / maxMin[0] * fixedHeight));
+      } else {
+        currentX = startX + W * i;
+        currentY = (startY - chatBeans[i].y / maxMin[0] * fixedHeight);
+        path.lineTo(currentX, currentY);
+      }
+
+      // currentX = startX + W * i;
+      // preX = startX + W * (i - 1);
+
+      // preY = (startY - chatBeans[i - 1].y / maxMin[0] * fixedHeight);
+      // currentY = (startY - chatBeans[i].y / maxMin[0] * fixedHeight);
+
+      // path.cubicTo(
+      //               (preX + currentX) / 2, preY,
+      //               (preX + currentX) / 2, currentY,
+      //               currentX, currentY
+      //             );
+    }
+
+    //对路径进行截取，实现动画
+    // var pathMetrics = path.computeMetrics(forceClosed: true);
+    // var list = pathMetrics.toList();
+    // var lengths = value * list.length.toInt();
+    // Path newPath = new Path();
+    // for (int i = 0; i < lengths; i++) {
+    //     var extractPath =list[i].extractPath(0, list[i].length * value, startWithMoveTo: true);
+    //     newPath.addPath(extractPath, Offset(0, 0));
+    //   }
+    // canvas.drawPath(newPath, paint);
+
     // for(int i = 0;i<10;i++){
     //   pointsList.add(DrawingPoints(
     //     points: Offset(40.0, 40.0),
@@ -29,37 +163,28 @@ class DemoPainter extends CustomPainter {
     //       ..isAntiAlias = true));
     // }
 
-    var paint = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round
-      ..color = Colors.green
-      ..strokeWidth = (1.0)
-      ..isAntiAlias = true;
-    
-    
     // canvas.drawLine(
     //     Offset(10.toDouble(),10.toDouble()), Offset(100.toDouble(),100.toDouble()), paint);
     // canvas.drawLine(
     // Offset(100.toDouble(),100.toDouble()), Offset(150.toDouble(),100.toDouble()), paint);
 
-    Path path = Path();
-    path.moveTo(100, 100);
-    path.lineTo(200, 200);
-    path.lineTo(250, 200);
-    path.lineTo(200, 250);
-    canvas.drawPath(path, paint);
+    // Path path = Path();
+    // path.moveTo(100, 100);
+    // path.lineTo(200, 200);
+    // path.lineTo(250, 200);
+    // path.lineTo(200, 250);
+    // canvas.drawPath(path, paint);
 
-
-    // var pathMetrics = path.computeMetrics(forceClosed: true);
-    // var list = pathMetrics.toList();
-    // var length = value * list.length.toInt();
-    // Path newPath = new Path();
-    // for (int i = 0; i < length; i++) {
-    //   var extractPath =list[i].extractPath(0, list[i].length * value, startWithMoveTo: true);
-    //   newPath.addPath(extractPath, Offset(0, 0));
-    // }
-    // canvas.drawPath(newPath, paint);
-
+    var pathMetrics = path.computeMetrics(forceClosed: false);
+    var list = pathMetrics.toList();
+    var dlength = value * list.length.toInt();
+    Path newPath = new Path();
+    for (int i = 0; i < dlength; i++) {
+      var extractPath =
+          list[i].extractPath(0, list[i].length * value, startWithMoveTo: true);
+      newPath.addPath(extractPath, Offset(0, 0));
+    }
+    canvas.drawPath(newPath, paint);
   }
 
   @override
@@ -76,12 +201,17 @@ class DemoWidget extends StatefulWidget {
 class _DemoWidgetState extends State<DemoWidget>
     with SingleTickerProviderStateMixin {
   AnimationController _controller;
+  double value;
+  List<Point> dataList;
 
   @override
   Widget build(BuildContext context) {
     //绘制Wight
+
     return CustomPaint(
-      painter: DemoPainter(),
+      size: Size(100, 200),
+      painter: DemoPainter(
+          chatBeans: [Point(y: 50.0), Point(y: 70.0), Point(y: 80.0)], value: _controller.value),
     );
   }
 
@@ -95,25 +225,28 @@ class _DemoWidgetState extends State<DemoWidget>
   @override
   void initState() {
     //动画启动
-    // _controller =
-    //     AnimationController(vsync: this, duration: Duration(milliseconds: 2000))
-    //       ..repeat()
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 2000))
+          ..repeat()
+          ..addListener(() {
+            setState(() {});
+          });
+    // _controller = AnimationController(
+    //     vsync: this, duration: Duration(milliseconds: 2000));
+    // Tween(
+    //         begin: 0.0,
+    //         end: 1.0)
+    //     .animate(_controller)
+    //       ..addStatusListener((status) {
+    //         if (status == AnimationStatus.completed) {
+    //           print('绘制完成');
+    //         }
+    //       })
     //       ..addListener(() {
+    //         value = _controller.value; //当前动画值
     //         setState(() {});
     //       });
-    _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 2000));
-      Tween(begin: 0.0, end: Duration(milliseconds: 2000).inMilliseconds.toDouble())
-          .animate(_controller)
-            ..addStatusListener((status) {
-              if (status == AnimationStatus.completed) {
-                print('绘制完成');
-              }
-            })
-            ..addListener(() {
-              var value = _controller.value;//当前动画值
-              setState(() {});
-            });
-      _controller.forward();
+     _controller.forward();
     super.initState();
   }
 }
